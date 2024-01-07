342890bba47518d71f9df9e3521a3f51b3733ea4
Shihab <mdshihabhossain1512@gmail.com>
2024-01-07 11:51:03 +0600

    login w google

--- ecommerce-app/src/app/page.tsx
+++ ecommerce-app/src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Shihab Again</h1>
+    <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
+      <div className="text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-lg">
+          Login with Google
+        </button>
+      </div>
     </div>
   );
 }
